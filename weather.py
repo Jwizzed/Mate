9751a8808ab888ac8b5ce15de8c654bf7a7fa272
Krittin SETDHAVANICH <krittin.set@ku.th>
2022-11-30 21:57:13 +0700

    delete one funcion in weather.py

--- weather.py
+++ weather.py
@@ -65,32 +65,3 @@ class Weather:
             _list.append(
                 "No need to bring an umbrella, There is no rain in 12 hours.")
         return _list
-
-    def get_information(self) -> list:
-        """Show the information of the weather."""
-        weather_params = {
-            "lat": self.lat,
-            "lon": self.lon,
-            "appid": API_KEY,
-            "exclude": "current,minutely,daily"
-        }
-
-        response = requests.get(OWN_ENDPOINT, params=weather_params, timeout=5)
-        weather_data = response.json()
-        weather_slice = weather_data["hourly"][:12]
-        _list = []
-        for index, hour_data in enumerate(weather_slice):
-            _dict = dict(temp=hour_data["temp"] + 272.15,
-                         feels_like=hour_data["feels_like"] + 272.15,
-                         pressure=hour_data["pressure"],
-                         humidity=hour_data["humidity"],
-                         clouds=hour_data["clouds"],
-                         wind_speed=hour_data["wind_speed"],
-                         wind_deg=hour_data["wind_deg"],
-                         wind_gust=hour_data["wind_gust"])
-
-            _list.append(f"Hour {index + 1}")
-            for key, value in _dict.items():
-                _list.append(f"{key}: {value}")
-            _list.append("\n")
-        return _list
